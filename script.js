ce02d183ca3c83a8b3f3b48593a2b69cad8af8ee
LucasLecoadic <43577166+LucasLecoadic@users.noreply.github.com>
2019-03-05 09:37:23 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+//Spell #1
+
+$(".hide-button").click(function(){
+     $("#hide").toggle(); 
+});
+
+
+
+//Spell #2
+
+$(".slide-button").click(function(){
+    $("#slide").slideToggle();
+});
+
+//Spell #3
+
+$(".fade-button").click(function(){
+    $("#fade").fadeToggle(); 
+});
+    
+
